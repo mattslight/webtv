13babca26084dd51c98237ce706e62be8ee588d6
Matthew Slight <matthew@kryptolabs.com>
2017-02-28 16:22:52 +0400

    added to code to dynamically load json models and loop ui to show categories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require("fs");
+
+var categories = JSON.parse(fs.readFileSync('./docs/categories.json', 'utf8'));
+var channels = JSON.parse(fs.readFileSync('./docs/channels.json', 'utf8'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Web.TV' });
+  res.render('index', { title: 'Web.TV', categories: categories });
 });
 
 module.exports = router;
+
+
