2885e325369c99228b44c64089c3601a17bf1bf1
Matthew Slight <matthew@kryptolabs.com>
2017-02-28 17:47:42 +0400

    dynamically load channels from category dict

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require("fs");
+var fs = require('fs');
 
 var categories = JSON.parse(fs.readFileSync('./docs/categories.json', 'utf8'));
 var channels = JSON.parse(fs.readFileSync('./docs/channels.json', 'utf8'));
+var catChan = {};
+
+
+channels.forEach(function(channel) {
+    var ls = catChan[channel['categoryId']];
+    if (ls === undefined) {
+        ls = [channel]
+    } else {
+        ls.push(channel)
+    }
+    catChan[channel['categoryId']] = ls
+});
+
+console.log(catChan);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Web.TV', categories: categories });
+  res.render('index', { title: 'Web.TV', categories: categories, channels: channels,  catChan: catChan});
 });
 
 module.exports = router;
